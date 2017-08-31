87e74d465ba1fb7de0db2ad099ef8260a1e0ab39
James Nash <james.nash@wipro.com>
2017-08-31 08:53:33 +0100

    Wired up basic Reveal.js code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const gulp = require('gulp');
+const del = require('del');
 const sass = require('gulp-sass');
 
 function absPath(relativePath){
@@ -8,8 +9,27 @@ function absPath(relativePath){
 
 const paths = {
   dist: absPath('dist/'),
-  reveal: absPath('node_modules/reveal.js/')
+  reveal: {
+    base: absPath('node_modules/reveal.js/')
+  },
+  sass: {
+    src: absPath('src/sass/style.scss')
+  }
 }
+paths.reveal.cssFile = path.resolve(paths.reveal.base, 'css/reveal.css'),
+paths.reveal.jsGlob = path.resolve(paths.reveal.base, 'js/**/*'),
+paths.reveal.libGlob = path.resolve(paths.reveal.base, 'lib/**/*'),
+paths.reveal.pluginGlob = path.resolve(paths.reveal.base, 'plugin/**/*'),
+paths.reveal.sassThemeBase = path.resolve(paths.reveal.base, 'css/theme/')
+
+
+
+// Nuke build dir
+gulp.task('clean', function(){
+  return del(path.resolve(paths.dist, '**/*'));
+});
+
+
 
 // Copy HTML to output
 gulp.task('html', function(){
@@ -20,21 +40,24 @@ gulp.task('html', function(){
 // Copy Reveal.js stuff output
 gulp.task('reveal', function(){
   return gulp.src([
-    path.resolve(paths.reveal, 'js/**/*'),
-    path.resolve(paths.reveal, 'lib/**/*'),
-    path.resolve(paths.reveal, 'plugin/**/*')
+    paths.reveal.cssFile,
+    paths.reveal.jsGlob,
+    paths.reveal.libGlob,
+    paths.reveal.pluginGlob
   ], {
-    base: paths.reveal,
+    base: paths.reveal.base,
   })
     .pipe(gulp.dest(path.resolve(paths.dist, 'reveal/')));
 });
 
 // Compile SASS
 gulp.task('sass', function(){
-  return gulp.src(absPath('src/sass/style.scss'))
-    .pipe(sass())
+  return gulp.src(paths.sass.src)
+    .pipe(sass({
+      includePaths: [paths.reveal.sassThemeBase]
+    }))
     .pipe(gulp.dest(paths.dist));
 });
 
 
-gulp.task('default', gulp.parallel('html', 'sass', 'reveal'));
+gulp.task('default', gulp.series('clean', gulp.parallel('html', 'sass', 'reveal')));
